a160274b2dc28d7cbc73fea5208a7800a54e5782
Vinic <vinic@gamestore.com>
2026-02-09 12:28:51 +0000

    rev. tests

--- asyncviews/tests.py
+++ asyncviews/tests.py
@@ -1,10 +1,15 @@
-from django.test import AsyncTestCase
-from django.urls import reverse
-from httpx import AsyncClient
+from django.test import TestCase, Client
 
-class AsyncViewTests(AsyncTestCase):
-    async def test_async_view(self):
-        async with AsyncClient() as client:
-            response = await client.get(reverse('async_counter'))
-            self.assertEqual(response.status_code, 200)
-            self.assertContains(response, "Non-blocking time counter started")
+
+class AsyncViewTests(TestCase):
+    def test_async_view(self):
+        client = Client()
+        response = client.get('/async-counter/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Non-blocking time counter started")
+    
+    def test_sync_view(self):
+        client = Client()
+        response = client.get('/sync-counter/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Blocking time counter finished")
